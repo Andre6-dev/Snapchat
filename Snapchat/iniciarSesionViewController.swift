671771ad4df757ace5b92a6e827310f120355321
Andre Gallegos Quenaya <andre.gallegos@tecsup.edu.pe>
2022-05-26 19:44:24 -0500

    Primer commit, usando la autenticacion basica de Firebase, agregando un usuario y comprobando que funcione.

---
+++ Snapchat/iniciarSesionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Mac 05 on 26/05/22.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func IniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando iniciar sesion")
+            if error != nil {
+                print("Se presento el siguiente error: \(error)")
+            } else {
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+}
+
